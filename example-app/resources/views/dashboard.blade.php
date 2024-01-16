0ef49ae4cbe3d86299e6089d393ce522a2f84f0c
Adilson Fernando Neves Ornellas <ornellas00@hotmail.com>
2024-01-16 11:13:43 -0300

    feat: estilização dos formularios com tailwindcss

--- example-app/resources/views/dashboard.blade.php
+++ example-app/resources/views/dashboard.blade.php
@@ -1,65 +1,83 @@
 <x-app-layout>
     <div>
-        <div class="row">
-            @if(session('msg'))
-            <p class="msg">{{ session('msg') }}</p>
+        @if(session('msg'))
+        <p class="bg-green-400 text-white font-bold py-2 px-4 text-center">{{ session('msg') }}</p>
+        @endif
+    </div>
+    <section class=" flex flex-col justify-center items-center">
+        <div class=" w-2/3">
+            <form class=" flex flex-col justify-center items-center gap-2" action="/dashboard" method="POST">
+                <h1 class="text-center font-bold">Cadastre um novo contato</h1>
+                @csrf
+                <div class=" flex gap-1 items-center w-full">
+                    <label for="nome">Nome:</label>
+                    <input class=" rounded-lg w-full" type="text" id="nome" name="nome" placeholder=" Digite o nome ">
+                </div>
+                <div class=" flex gap-1 items-center w-full">
+                    <label for="numero">Numero:</label>
+                    <input class=" rounded-lg w-full" type="text" id="numero" name="numero"
+                        placeholder="Digite o numero">
+                </div>
+                <div class=" flex gap-1  justify-center  items-center w-full">
+                    <label for="whatsapp">Tem whatsapp?</label>
+                    <select class=" rounded-lg" name="whatsapp" id="whatsapp">
+                        <option value="0">Não</option>
+                        <option value="1">Sim</option>
+                    </select>
+                </div>
+                <input class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded w-full text-center"
+                    type="submit" value="Salvar">
+            </form>
+            <br />
+            <h2>Pesquise por um contato especifico</h2>
+            <form class="flex gap-1" action="/dashboard" method="GET">
+                <input class=" rounded-lg w-4/5" type="text" id="search" name="search" placeholder="Procurar...">
+                <input class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded " type="submit"
+                    value="Procurar">
+            </form>
+            @if($search)
+            <br />
+            <h2 class="text-center">Buscando por: {{ $search }}</h2>
+            <br />
+            @else
+            <br />
+            <h2 class="text-center">Todos seus contatos:</h2>
+            <br />
+            @endif
+
+            <div class="flex flex-col gap-3">
+                @foreach($contatos as $contato)
+                <div class="flex flex-col gap-1">
+                    <h5 class="card-title">Nome: {{ $contato->nome }}</h5>
+                    <h5 class="card-title">Numero: {{ $contato->numero }}</h5>
+                    <h5 class="card-title">Tem whatsapp: {{ $contato->whatsapp == 1 ? 'Sim' : 'Não' }}</h5>
+                    <form class="flex gap-1" action="/dashboard/{{ $contato->id }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <a class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full text-center"
+                            href="/dashboard/edit/{{ $contato->id }}">Editar</a>
+                        <button
+                            class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded w-full text-center"
+                            type="submit">
+                            Deletar
+                        </button>
+                    </form>
+                </div>
+                @endforeach
+            </div>
+            @if(count($contatos) == 0)
+            @if(count($contatos) == 0 && $search)
+            <div class="flex flex-col gap-2">
+                <p>Não foi possível encontrar nenhum contato com {{ $search }}!
+                </p>
+                <a class="bg-gray-300 hover:bg-gray-500 text-white font-bold py-2 px-4 rounded text-center "
+                    href=" /dashboard">Ver
+                    todos</a>
+            </div>
+            @elseif(count($contatos) == 0)
+            <p>Não há nenhum contato</p>
+            @endif
             @endif
         </div>
-    </div>
-    <form action="/dashboard" method="POST">
-        <h1>Cadastre um novo contato</h1>
-        @csrf
-        <div>
-            <label for="nome">Nome:</label>
-            <input type="text" id="nome" name="nome" placeholder="Nome do contato">
-        </div>
-        <div>
-            <label for="numero">Numero:</label>
-            <input type="text" id="numero" name="numero" placeholder="Digite o numero">
-        </div>
-        <div>
-            <label for="whatsapp">Tem whatsapp?</label>
-            <select name="whatsapp" id="whatsapp">
-                <option value="0">Não</option>
-                <option value="1">Sim</option>
-            </select>
-        </div>
-        <input type="submit" value="Salvar">
-    </form>
-    <br />
-    <br />
-    <h2>Pesquise por um contato especifico</h2>
-    <form action="/dashboard" method="GET">
-        <input type="text" id="search" name="search" placeholder="Procurar...">
-    </form>
-    @if($search)
-    <h2>Buscando por: {{ $search }}</h2>
-    <br />
-    @else
-    <br />
-    <h2>Todos seus contatos:</h2>
-    <br />
-    @endif
-
-    @foreach($contatos as $contato) <div class="card col-md-3">
-        <h5 class="card-title">nome: {{ $contato->nome }}</h5>
-        <h5 class="card-title">numero: {{ $contato->numero }}</h5>
-        <h5 class="card-title">Tem whatsapp: {{ $contato->whatsapp }}</h5>
-        <a href="/dashboard/edit/{{ $contato->id }}">Editar</a>
-        <form action="/dashboard/{{ $contato->id }}" method="POST">
-            @csrf
-            @method('DELETE')
-            <button type="submit"> Deletar </button>
-        </form>
-    </div>
-    @endforeach
-
-    @if(count($contatos) == 0)
-    @if(count($contatos) == 0 && $search)
-    <p>Não foi possível encontrar nenhum contato com {{ $search }}! <a href="/dashboard">Ver todos</a></p>
-    @elseif(count($contatos) == 0)
-    <p>Não há nenhum contato</p>
-    @endif
-    @endif
-
+    </section>
 </x-app-layout>
--- example-app/resources/views/edit.blade.php
+++ example-app/resources/views/edit.blade.php
@@ -1,23 +1,32 @@
 <x-app-layout>
-    <h1>Editando: {{ $contato->nome }}</h1>
-    <form action="/dashboard/update/{{ $contato->id }}" method="POST" enctype="multipart/form-data">
-        @csrf
-        @method('PUT')
-        <div>
-            <label for="nome">Nome:</label>
-            <input type="text" id="nome" name="nome" placeholder="Nome do contato" value="{{ $contato->nome }}">
+    <section class=" flex flex-col justify-center items-center">
+        <div class=" w-2/3">
+            <form class=" flex flex-col justify-center items-center gap-2"
+                action=" /dashboard/update/{{ $contato->id }}" method="POST" enctype="multipart/form-data">
+                <h1 class="text-center font-bold">Editando: {{ $contato->nome }}</h1>
+                @csrf
+                @method('PUT')
+                <div class=" flex gap-1 items-center w-full">
+                    <label for="nome">Nome:</label>
+                    <input class=" rounded-lg w-full" type="text" id="nome" name="nome" placeholder="Nome do contato"
+                        value="{{ $contato->nome }}">
+                </div>
+                <div class=" flex gap-1 items-center w-full">
+                    <label for="numero">Numero:</label>
+                    <input class=" rounded-lg w-full" type="text" id="numero" name="numero"
+                        placeholder="Digite o numero" value="{{ $contato->numero }}">
+                </div>
+                <div class=" flex gap-1  justify-center  items-center w-full">
+                    <label for=" whatsapp">Tem whatsapp?</label>
+                    <select class=" rounded-lg" name="whatsapp" id="whatsapp">
+                        <option value="0">Não</option>
+                        <option value="1" {{ $contato->whatsapp == 1 ? "selected='selected'" : "" }}>Sim</option>
+                    </select>
+                </div>
+                <input class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded w-full text-center"
+                    type="
+                    submit" value="Salvar edição">
+            </form>
         </div>
-        <div>
-            <label for="numero">Numero:</label>
-            <input type="text" id="numero" name="numero" placeholder="Digite o numero" value="{{ $contato->numero }}">
-        </div>
-        <div>
-            <label for=" whatsapp">Tem whatsapp?</label>
-            <select name="whatsapp" id="whatsapp">
-                <option value="0">Não</option>
-                <option value="1" {{ $contato->whatsapp == 1 ? "selected='selected'" : "" }}>Sim</option>
-            </select>
-        </div>
-        <input type="submit" value="Salvar edição">
-    </form>
+    </section>
 </x-app-layout>
