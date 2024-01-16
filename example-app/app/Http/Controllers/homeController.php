b01cc588a8d57e278f1ee22cdc8288a82f30978c
Adilson Fernando Neves Ornellas <ornellas00@hotmail.com>
2024-01-16 00:12:04 -0300

    feat: pagina home ja fazendo o get dos contatos do usuario logado e fazendo o post de novos contatos, crud pela metade, ainda sem estilização

--- example-app/app/Http/Controllers/homeController.php
+++ example-app/app/Http/Controllers/homeController.php
@@ -3,10 +3,42 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\contact;
 
-class homeController extends Controller
+class HomeController extends Controller
 {
-    public function index() {
+    public function index(){
         
+        $user = auth()->user();
+        
+        $search = request('search');
+
+        if($search) {
+
+            $contatos = contact::where([
+                ['nome', 'like', '%'.$search.'%']
+            ])->get();
+
+        } else {
+            // $contatos = contact::all();
+            $contatos = $user->contatos;
+        }
+        
+        return view('dashboard',['contatos' => $contatos, 'search' => $search]);
     }
+    
+    public function store(Request $request) {
+
+        $user = auth()->user();
+        $contato = new contact;
+
+        $contato->nome = $request->nome;
+        $contato->numero = $request->numero;
+        $contato->whatsapp = $request->whatsapp;
+        $contato->user_id = $user->id;
+        $contato->save();
+        
+        return redirect('/dashboard')->with('msg', 'Contato adicionado com sucesso!');
+    }
+    
 }
--- example-app/resources/views/dashboard.blade.php
+++ example-app/resources/views/dashboard.blade.php
@@ -1,3 +1,59 @@
 <x-app-layout>
+    <div>
+        <div class="row">
+            @if(session('msg'))
+            <p class="msg">{{ session('msg') }}</p>
+            @endif
+        </div>
+    </div>
+    <form action="/dashboard" method="POST">
+        <h1>Cadastre um novo contato</h1>
+        @csrf
+        <div>
+            <label for="nome">Nome:</label>
+            <input type="text" id="nome" name="nome" placeholder="Nome do contato">
+        </div>
+        <div>
+            <label for="numero">Numero:</label>
+            <input type="text" id="numero" name="numero" placeholder="Digite o numero">
+        </div>
+        <div>
+            <label for="whatsapp">Tem whatsapp?</label>
+            <select name="whatsapp" id="whatsapp">
+                <option value="0">Não</option>
+                <option value="1">Sim</option>
+            </select>
+        </div>
+        <input type="submit" value="Salvar Contato">
+    </form>
+    <br />
+    <br />
+    <h2>Pesquise por um contato especifico</h2>
+    <form action="/dashboard" method="GET">
+        <input type="text" id="search" name="search" placeholder="Procurar...">
+    </form>
+    @if($search)
+    <h2>Buscando por: {{ $search }}</h2>
+    <br />
+    @else
+    <br />
+    <h2>Todos seus contatos:</h2>
+    <br />
+    @endif
+
+    @foreach($contatos as $contato) <div class="card col-md-3">
+        <h5 class="card-title">nome: {{ $contato->nome }}</h5>
+        <h5 class="card-title">numero: {{ $contato->numero }}</h5>
+        <h5 class="card-title">Tem whatsapp: {{ $contato->whatsapp }}</h5>
+    </div>
+    @endforeach
+
+    @if(count($contatos) == 0)
+    @if(count($contatos) == 0 && $search)
+    <p>Não foi possível encontrar nenhum contato com {{ $search }}! <a href="/dashboard">Ver todos</a></p>
+    @elseif(count($contatos) == 0)
+    <p>Não há nenhum contato</p>
+    @endif
+    @endif
 
 </x-app-layout>
